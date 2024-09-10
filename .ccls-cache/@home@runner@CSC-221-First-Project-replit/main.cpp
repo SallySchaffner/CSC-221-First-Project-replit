1034eb22c5dc61a7355bc77c8db65c21a0bf9f7b
Sally Schaffner <151073463+SallySchaffner@users.noreply.github.com>
2024-09-10 14:20:13 +0000

    Cleared out main.cpp

--- .ccls-cache/@home@runner@CSC-221-First-Project-replit/main.cpp
+++ .ccls-cache/@home@runner@CSC-221-First-Project-replit/main.cpp
@@ -1,13 +1,8 @@
-// Display my initials
+// 
 #include <iostream>
 using namespace std;
 
 int main() 
 { 
-  char first, middle, last;
-  first = 'S';
-  middle = 'K';
-  last = 'S';
-  cout << first << middle << last << endl;
-  return 0;
+  
 }
